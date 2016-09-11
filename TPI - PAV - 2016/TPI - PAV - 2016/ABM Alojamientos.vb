b2ddc8077b2ee6bf14245b6df4ec9a2a81dc51eb
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2016-09-11 19:51:07 -0300

    Avance ABM alojamientos

--- TPI - PAV - 2016/TPI - PAV - 2016/ABM Alojamientos.vb
+++ TPI - PAV - 2016/TPI - PAV - 2016/ABM Alojamientos.vb
@@ -3,8 +3,7 @@
 
     Private Sub ABM_Alojamientos_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         carga_combo(Me.cmbTipoDoc, Me.leo_tabla("TiposDocumento"), "idTipoDocumento", "nombre")
-        'carga_lista(Me.leo_alojamientos(null))
-        'blanqueo fecha salida
+        'carga_lista(Me.leo_alojamientos(null)) COMPLETAR
         dtpSalida.Format = DateTimePickerFormat.Custom
         dtpSalida.CustomFormat = "    "
     End Sub
@@ -14,6 +13,32 @@
         combo.ValueMember = pk
         combo.DisplayMember = descripcion
     End Sub
+
+    Private Sub carga_lista(ByVal tabla As Data.DataTable)
+        Me.dgvAlojamientos.DataSource = tabla
+        'COMPLETAR
+    End Sub
+
+    Private Function leo_alojamientos(ByVal porDoc As Boolean)
+        Dim conexion As New OleDb.OleDbConnection
+        Dim cmd As New OleDb.OleDbCommand
+        Dim tabla As New Data.DataTable
+
+        conexion.ConnectionString = Me.string_conexion
+        conexion.Open()
+        cmd.Connection = conexion
+        cmd.CommandType = CommandType.Text
+        If porDoc Then
+            cmd.CommandText = "select fechaInicioAlojamiento, fechaFinAlojamiento, nroHabitacion" &
+                        "from Alojamientos where tipoDoc=" & cmbTipoDoc.SelectedValue & " AND numeroDoc=" & txtNroDoc.Text
+        Else
+            cmd.CommandText = "select fechaInicioAlojamiento, fechaFinAlojamiento, nroHabitacion" &
+                "from Alojamientos order by fechaInicioAlojamiento DESC"
+        End If
+        tabla.Load(cmd.ExecuteReader())
+
+        Return tabla
+    End Function
 
     Private Function leo_tabla(ByVal nombre_tabla As String) As Data.DataTable
         Dim conexion As New OleDb.OleDbConnection
@@ -32,5 +57,13 @@
 
     Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
         Me.Close()
+    End Sub
+
+    Private Sub btnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        Me.limpiar()
+    End Sub
+
+    Private Sub limpiar()
+        'Completar
     End Sub
 End Class
