8f5e153636953c3cd1f7910d0a0408b74566aa1b
MauriCasares <mauricd@hotmail.com>
2016-09-17 23:09:04 -0300

    Clase ACCESO

--- TPI - PAV - 2016/TPI - PAV - 2016/ABM Clientes.vb
+++ TPI - PAV - 2016/TPI - PAV - 2016/ABM Clientes.vb
@@ -1,5 +1,7 @@
 ﻿Public Class ABM_Clientes
+    Dim acceso As AccesoBD = AccesoBD.instancia
     Dim cadena_conexion As String = ConexionBD.Instancia.StringConexion
+
     Enum estado
         insertar
         modificar
@@ -13,7 +15,93 @@
     End Enum
 
 
+    Private Sub ABM_Clientes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.cargarCombo("TiposDocumento", "idTipoDocumento", "nombre", Me.cmb_tipoDoc)
+        Me.cargarGrilla(estadoBusqueda.todo)
+    End Sub
 
+
+#Region "Subrutinas"
+    'CARGAR COMBO
+    Private Sub cargarCombo(ByVal nombre_tabla As String, ByVal pk As String, ByVal descripcion As String, ByRef combo As ComboBox)
+        Dim tabla As Data.DataTable = acceso.query("SELECT * FROM " + nombre_tabla)
+
+        combo.DataSource = tabla
+        combo.DisplayMember = descripcion
+        combo.ValueMember = pk
+    End Sub
+
+    'CARGAR GRILLA
+    Private Sub cargarGrilla(ByVal buscarEn As estadoBusqueda)
+        Me.grid_clientes.Rows.Clear()
+
+        Dim consultaSQL As String = "SELECT * FROM Clientes C JOIN TiposDocumento T ON  C.tipoDocumento = T.idTipoDocumento "
+
+        If buscarEn = estadoBusqueda.tipoDocumento Then
+            consultaSQL &= " WHERE tipoDocumento = " & Me.cmb_tipoDoc.SelectedValue
+
+        ElseIf buscarEn = estadoBusqueda.nroDocumento Then
+            consultaSQL &= " WHERE nroDocumento = " & Me.txt_nroDoc.Text & " AND tipoDocumento = " & Me.cmb_tipoDoc.SelectedValue
+        End If
+
+        Dim tabla As Data.DataTable = acceso.query(consultaSQL)
+
+        Dim i As Integer
+        For i = 0 To tabla.Rows.Count() - 1
+
+            Me.grid_clientes.Rows.Add() 'current row es la fila actual y cell es la celda de esa fila
+            Me.grid_clientes.Rows(i).Cells("col_nombre").Value = tabla.Rows(i)("nombre")
+            Me.grid_clientes.Rows(i).Cells("col_apellido").Value = tabla.Rows(i)("apellido")
+            Me.grid_clientes.Rows(i).Cells("col_tipoDocumento").Value = tabla.Rows(i)(7)
+            Me.grid_clientes.Rows(i).Cells("col_nroDocumento").Value = tabla.Rows(i)("nroDocumento")
+        Next
+    End Sub
+
+    'INSERTAR CLIENTE
+    Private Sub insertar()
+        Dim sentenciaSQL As String = "INSERT INTO Clientes (apellido, nombre, nroDocumento, tipoDocumento, fechaNacimiento, telefono) " _
+           & "VALUES ('" & txt_apellido.Text & "', '" & txt_nombre.Text & "', " & txt_nroDoc.Text & ", '" & cmb_tipoDoc.SelectedValue & "',getDate(), " & txt_telefono.Text & ")"
+        'dtpEstimada.Value.Date
+
+        acceso.nonQuery(sentenciaSQL)
+        MessageBox.Show("Se registró exitosamente.")
+        Me.cargarGrilla(estadoBusqueda.todo)
+    End Sub
+
+    'BUSCAR CLIENTE
+    Private Sub cmd_buscar_Click(sender As Object, e As EventArgs) Handles cmd_buscar.Click
+        If Me.txt_nroDoc.Text = "" Then
+            Me.cargarGrilla(estadoBusqueda.tipoDocumento)
+        Else
+            Me.cargarGrilla(estadoBusqueda.nroDocumento)
+        End If
+    End Sub
+
+    'MODIFICAR CLIENTE
+    Private Sub modificar()
+        Dim sentenciaSQL As String = ""
+        sentenciaSQL &= "UPDATE Clientes "
+        sentenciaSQL &= "SET nombre = '" & Me.txt_nombre.Text & "'"
+        sentenciaSQL &= " , apellido = '" & Me.txt_apellido.Text & "'"
+        sentenciaSQL &= " , telefono = '" & Me.txt_telefono.Text & "'"
+        sentenciaSQL &= " , fechaNacimiento = '" & Me.dtpEstimada.Value.Date & "'"
+        sentenciaSQL &= " WHERE nroDocumento = " & Me.txt_nroDoc.Text
+        sentenciaSQL &= " AND tipoDocumento = " & Me.cmb_tipoDoc.SelectedValue
+
+        acceso.nonQuery(sentenciaSQL)
+
+        MessageBox.Show("Se modificó correctamente.")
+        Me.cargarGrilla(estadoBusqueda.todo)
+    End Sub
+
+
+
+#End Region
+
+
+#Region "Funciones"
+
+    'VALIDAR
     Private Function Validar() As Boolean
         If txt_apellido.Text = "" Then
             MessageBox.Show("Debe completar el campo Apellido", "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop)
@@ -42,11 +130,32 @@
         End If
         Return True
     End Function
+
+
+    Private Function validarExistencia() As Boolean
+        Dim sentenciaSQL As String = "SELECT * FROM Clientes "
+        sentenciaSQL &= "WHERE tipoDocumento = " & Me.cmb_tipoDoc.SelectedValue & " AND nroDocumento = " & txt_nroDoc.Text
+
+        Dim tabla As DataTable = acceso.query(sentenciaSQL)
+
+        If tabla.Rows.Count() = 0 Then
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+
+#End Region
+
+
+#Region "Eventos"
+
+    ' CLICK EN REGISTRAR
     Private Sub cmd_registrar_Click(sender As Object, e As EventArgs) Handles cmd_registrar.Click
 
         If Me.Validar = True Then
             If condicion_estado = estado.insertar Then
-                If Me.validar_existencia() = False Then
+                If Me.validarExistencia() = False Then
                     Me.insertar()
                 Else
                     MessageBox.Show("Ya existe este Cliente")
@@ -58,116 +167,9 @@
             End If
             Me.cmd_limpiar.PerformClick()
         End If
-
-
     End Sub
 
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.cargar_combo("TiposDocumento", "idTipoDocumento", "nombre", Me.cmb_tipoDoc)
-        Me.cargar_grilla(estadoBusqueda.todo)
-
-    End Sub
-
-    Private Sub cargar_combo(ByVal nombre_tabla As String, ByVal pk As String, ByVal descripcion As String, ByRef combo As ComboBox)
-        Dim conexion As New Data.OleDb.OleDbConnection
-        Dim cmd As New Data.OleDb.OleDbCommand
-        Dim tabla As New Data.DataTable
-
-        Dim sql As String = "SELECT * FROM " + nombre_tabla
-        conexion.ConnectionString = cadena_conexion
-        conexion.Open()
-        cmd.Connection = conexion
-        cmd.CommandType = CommandType.Text
-        cmd.CommandText = sql
-        tabla.Load(cmd.ExecuteReader)
-        conexion.Close()
-
-        combo.DataSource = tabla
-        combo.DisplayMember = descripcion
-        combo.ValueMember = pk
-    End Sub
-
-    Private Sub cargar_grilla(ByVal buscarEn As estadoBusqueda)
-        Me.grid_clientes.Rows.Clear()
-        Dim conexion As New Data.OleDb.OleDbConnection
-        Dim comando As New Data.OleDb.OleDbCommand
-        Dim tabla As New Data.DataTable
-
-        Dim sql As String = "SELECT * FROM Clientes C JOIN TiposDocumento T ON  C.tipoDocumento = T.idTipoDocumento "
-
-        If buscarEn = estadoBusqueda.tipoDocumento Then
-            sql = sql & " WHERE tipoDocumento = " & Me.cmb_tipoDoc.SelectedValue
-
-        ElseIf buscarEn = estadoBusqueda.nroDocumento Then
-            sql = sql & " WHERE nroDocumento = " & Me.txt_nroDoc.Text & " AND tipoDocumento = " & Me.cmb_tipoDoc.SelectedValue
-        End If
-
-
-
-        conexion.ConnectionString = cadena_conexion
-        conexion.Open()
-        comando.Connection = conexion
-        comando.CommandType = CommandType.Text
-        comando.CommandText = sql
-        tabla.Load(comando.ExecuteReader)
-        conexion.Close()
-
-        Dim i As Integer
-
-        For i = 0 To tabla.Rows.Count() - 1
-
-            Me.grid_clientes.Rows.Add() 'current row es la fila actual y cell es la celda de esa fila
-            Me.grid_clientes.Rows(i).Cells("col_nombre").Value = tabla.Rows(i)("nombre")
-            Me.grid_clientes.Rows(i).Cells("col_apellido").Value = tabla.Rows(i)("apellido")
-            Me.grid_clientes.Rows(i).Cells("col_tipoDocumento").Value = tabla.Rows(i)(7)
-            Me.grid_clientes.Rows(i).Cells("col_nroDocumento").Value = tabla.Rows(i)("nroDocumento")
-        Next
-
-    End Sub
-
-    Private Sub insertar()
-        Dim conexion As New OleDb.OleDbConnection
-        Dim cmd As New OleDb.OleDbCommand
-        Dim sql As String = "INSERT INTO Clientes (apellido, nombre, nroDocumento, tipoDocumento, fechaNacimiento, telefono) " _
-           & "VALUES ('" & txt_apellido.Text & "', '" & txt_nombre.Text & "', " & txt_nroDoc.Text & ", '" & cmb_tipoDoc.SelectedValue & "','" & dtpEstimada.Value.Date & "', " & txt_telefono.Text & ")"
-
-
-        conexion.ConnectionString = cadena_conexion
-        conexion.Open()
-        cmd.Connection = conexion
-        cmd.CommandType = CommandType.Text
-        cmd.CommandText = sql
-        cmd.ExecuteNonQuery()
-        conexion.Close()
-        MessageBox.Show("Se registró exitosamente.")
-        Me.cargar_grilla(estadoBusqueda.todo)
-    End Sub
-
-    Private Function validar_existencia() As Boolean
-        Dim conexion As New OleDb.OleDbConnection
-        Dim cmd As New OleDb.OleDbCommand
-        Dim tabla As New DataTable
-        Dim sql As String = ""
-        sql = "SELECT * FROM Clientes " _
-            & "WHERE tipoDocumento = " & Me.cmb_tipoDoc.SelectedValue & " AND nroDocumento = " & txt_nroDoc.Text
-
-        conexion.ConnectionString = cadena_conexion
-        conexion.Open()
-        cmd.Connection = conexion
-        cmd.CommandType = CommandType.Text
-        cmd.CommandText = sql
-
-        tabla.Load(cmd.ExecuteReader())
-        conexion.Close()
-
-        If tabla.Rows.Count() = 0 Then
-            Return False
-        Else
-            Return True
-        End If
-    End Function
-
+    'CLICK EN LIMPIAR
     Private Sub cmd_limpiar_Click(sender As Object, e As EventArgs) Handles cmd_limpiar.Click
         Me.condicion_estado = estado.insertar
         Me.txt_apellido.Text = ""
@@ -177,39 +179,38 @@
         Me.dtpEstimada.Value = Today
         Me.txt_nroDoc.Enabled = True
         Me.cmb_tipoDoc.Enabled = True
-        Me.cargar_grilla(estadoBusqueda.todo)
+        Me.cargarGrilla(estadoBusqueda.todo)
         Me.cmd_registrar.Text = "Registrar"
         Me.cmd_borrar.Enabled = False
         Me.cmd_borrar.Visible = False
-
     End Sub
 
-    Private Sub cmd_buscar_Click(sender As Object, e As EventArgs) Handles cmd_buscar.Click
-        If Me.txt_nroDoc.Text = "" Then
-            Me.cargar_grilla(estadoBusqueda.tipoDocumento)
-        Else
-            Me.cargar_grilla(estadoBusqueda.nroDocumento)
-        End If
-
+    'CLICK EN CANCELAR
+    Private Sub cmd_cancelar_Click(sender As Object, e As EventArgs) Handles cmd_cancelar.Click
+        Me.Close()
     End Sub
 
+    'CLICK EN BORRAR
+    Private Sub cmd_borrar_Click(sender As Object, e As EventArgs) Handles cmd_borrar.Click
+        Dim sql As String = "DELETE FROM Clientes WHERE nroDocumento = " & Me.txt_nroDoc.Text
+        sql &= " AND tipoDocumento = " & Me.cmb_tipoDoc.SelectedValue
+
+        Dim tabla As DataTable = acceso.query(sql)
+        MessageBox.Show("Se ha eliminado el tipo de Documento Satisfactoriamente")
+        Me.cargarGrilla(estadoBusqueda.todo)
+        Me.cmd_limpiar.PerformClick()
+    End Sub
+
+    'DOBLE CLICK SOBRE LA GRILLA
     Private Sub grid_clientes_CellContentDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles grid_clientes.CellContentDoubleClick
-        Dim conexion As New OleDb.OleDbConnection
-        Dim cmd As New OleDb.OleDbCommand
-        Dim tabla As New DataTable
-        Dim sql As String = ""
+        Dim sentenciaSQL As String = ""
 
-        sql = "SELECT * FROM Clientes C JOIN TiposDocumento T ON  C.tipoDocumento = T.idTipoDocumento " _
+        sentenciaSQL = "SELECT * FROM Clientes C JOIN TiposDocumento T ON  C.tipoDocumento = T.idTipoDocumento " _
             & "WHERE T.nombre = '" & Me.grid_clientes.CurrentRow.Cells("col_tipoDocumento").Value & "' AND C.nroDocumento = " & Me.grid_clientes.CurrentRow.Cells("col_nroDocumento").Value
 
 
-        conexion.ConnectionString = cadena_conexion
-        conexion.Open()
-        cmd.Connection = conexion
-        cmd.CommandType = CommandType.Text
-        cmd.CommandText = sql
-        tabla.Load(cmd.ExecuteReader())
-        conexion.Close()
+        Dim tabla As DataTable = acceso.query(sentenciaSQL)
+
         If tabla.Rows.Count() = 0 Then
             MessageBox.Show("El cliente requerido no existe.")
             Exit Sub
@@ -233,58 +234,5 @@
         Me.condicion_estado = estado.modificar
         Me.cmd_buscar.PerformClick()
     End Sub
-
-    Private Sub modificar()
-
-        Dim conexion As New OleDb.OleDbConnection
-        Dim cmd As New OleDb.OleDbCommand
-        Dim sql As String = ""
-
-        sql &= "UPDATE Clientes "
-        sql &= "SET nombre = '" & Me.txt_nombre.Text & "'"
-        sql &= " , apellido = '" & Me.txt_apellido.Text & "'"
-        sql &= " , telefono = '" & Me.txt_telefono.Text & "'"
-        sql &= " , fechaNacimiento = '" & Me.dtpEstimada.Value.Date & "'"
-        sql &= " WHERE nroDocumento = " & Me.txt_nroDoc.Text
-        sql &= " AND tipoDocumento = " & Me.cmb_tipoDoc.SelectedValue
-
-        conexion.ConnectionString = cadena_conexion
-        conexion.Open()
-        cmd.Connection = conexion
-        cmd.CommandType = CommandType.Text
-        cmd.CommandText = sql
-
-        cmd.ExecuteNonQuery()
-
-        conexion.Close()
-        MessageBox.Show("Se modificó correctamente.")
-        Me.cargar_grilla(estadoBusqueda.todo)
-
-    End Sub
-
-
-
-    Private Sub cmd_cancelar_Click(sender As Object, e As EventArgs) Handles cmd_cancelar.Click
-        Me.Close()
-    End Sub
-
-    Private Sub cmd_borrar_Click(sender As Object, e As EventArgs) Handles cmd_borrar.Click
-        Dim conexion As New OleDb.OleDbConnection
-        Dim cmd As New OleDb.OleDbCommand
-        Dim tabla As New DataTable
-        Dim sql As String = "DELETE FROM Clientes WHERE nroDocumento = " & Me.txt_nroDoc.Text
-        sql &= " AND tipoDocumento = " & Me.cmb_tipoDoc.SelectedValue
-
-        conexion.ConnectionString = cadena_conexion
-        conexion.Open()
-        cmd.Connection = conexion
-        cmd.CommandType = CommandType.Text
-        cmd.CommandText = sql
-        tabla.Load(cmd.ExecuteReader())
-        conexion.Close()
-        Me.cargar_grilla(estadoBusqueda.todo)
-        Me.cmd_limpiar.PerformClick()
-
-        MessageBox.Show("Se ha eliminado el tipo de Documento Satisfactoriamente")
-    End Sub
+#End Region
 End Class
---
+++ TPI - PAV - 2016/TPI - PAV - 2016/AccesoBD.vb
@@ -0,0 +1,72 @@
+﻿Public Class AccesoBD
+    Private pCadenaConexion As String
+    Private conexion As Object
+    Private comando As Object
+
+    Private Shared _instancia As AccesoBD = Nothing
+    Private Shared ReadOnly sync As New Object
+
+
+    'GETTER Y SETTER de cadena conexion
+    Public Property cadenaConexion As String
+        Get
+            Return Me.pCadenaConexion
+        End Get
+        Set(ByVal cadena As String)
+            Me.pCadenaConexion = cadena
+        End Set
+    End Property
+
+    'SINGLETON
+    Public Shared ReadOnly Property instancia As AccesoBD
+        Get
+            If _instancia Is Nothing Then
+                SyncLock sync
+                    If _instancia Is Nothing Then
+                        _instancia = New AccesoBD
+                        MessageBox.Show("La conexion a la Base de Datos se realizo correctamente.")
+                    End If
+                End SyncLock
+            End If
+            Return _instancia
+        End Get
+    End Property
+
+
+    'CONSTRUCTOR
+    Private Sub New()
+        Me.conexion = New OleDb.OleDbConnection
+        Me.comando = New OleDb.OleDbCommand()
+        Me.pCadenaConexion = "Provider=SQLNCLI11;Data Source=LAWEBSTORE-PC\SQLSERVER2014;Integrated Security=SSPI;Initial Catalog=hotelShaky"
+    End Sub
+
+
+    Public Function query(ByVal sentenciaSQL As String) As DataTable
+        Dim retorno As New DataTable
+        Me.conectar()
+        Me.comando.CommandText = sentenciaSQL
+
+        retorno.Load(comando.ExecuteReader())
+        cerrar()
+
+        Return retorno
+    End Function
+
+    Public Sub nonQuery(ByVal sentenciaSQL As String)
+        Me.conectar()
+        Me.comando.CommandText = sentenciaSQL
+        Me.comando.ExecuteNonQuery()
+        Me.cerrar()
+    End Sub
+
+    Private Sub cerrar()
+        Me.conexion.Close()
+    End Sub
+
+    Private Sub conectar()
+        Me.conexion.ConnectionString = Me.pCadenaConexion
+        Me.conexion.Open()
+        Me.comando.Connection = Me.conexion
+        Me.comando.CommandType() = CommandType.Text
+    End Sub
+End Class
