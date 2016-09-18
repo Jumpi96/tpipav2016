589c473abb95922851060a573a5d52f7e5afe6c1
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2016-09-18 19:26:14 -0300

    TiposHabitacion borrar + StringConexion agregado

--- TPI - PAV - 2016/TPI - PAV - 2016/Clases/AccesoBD.vb
+++ TPI - PAV - 2016/TPI - PAV - 2016/Clases/AccesoBD.vb
@@ -43,8 +43,8 @@
             Case "LAWEBSTORE-PC"
                 Me.pCadenaConexion = "Provider=SQLNCLI11;Data Source=LAWEBSTORE-PC\SQLSERVER2014;Integrated Security=SSPI;Initial Catalog=hotelShaky"
 
-                'Case "NOMBRE-COMPU-JUAMPI"
-                'Me.pCadenaConexion = "CADENA CONEXION MARCE"
+            Case "DESKTOP-2BQ04QL"
+                Me.pCadenaConexion = "Provider=SQLNCLI11;Data Source=DESKTOP-2BQ04QL;Integrated Security=SSPI;Initial Catalog=hotelShaky"
 
                 'Case "NOMBRE-COMPU-JULI"
                 'Me.pCadenaConexion = "CADENA CONEXION JULI"
