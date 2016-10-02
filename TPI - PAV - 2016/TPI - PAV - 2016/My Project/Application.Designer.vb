b8fcd3549dda529217c761ef3f3959f167e6fd60
Juli Rios <julirios299@gmail.com>
2016-10-02 20:30:31 -0300

    fjhkghkjhgkj
    yjurj

--- TPI - PAV - 2016/TPI - PAV - 2016/My Project/Application.Designer.vb
+++ TPI - PAV - 2016/TPI - PAV - 2016/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.TPI___PAV___2016.ABM_Habitación_X_Piso
+            Me.MainForm = Global.TPI___PAV___2016.menu
         End Sub
     End Class
 End Namespace
