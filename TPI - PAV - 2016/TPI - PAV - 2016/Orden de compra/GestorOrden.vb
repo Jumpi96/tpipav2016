b3dea6d948a151801f777831eca80b44d00a6834
luchogil96 <luchogil96@gmail.com>
2016-10-17 20:43:03 -0300

    Avances en GestorOrden
    Faltaria lo que pasa despues de que se genere la Orden y lo de
    imprimirla.
    Ya esta hecho la generacion de la orden en si y la actualizacion de
    stock.

--- TPI - PAV - 2016/TPI - PAV - 2016/Orden de compra/GestorOrden.vb
+++ TPI - PAV - 2016/TPI - PAV - 2016/Orden de compra/GestorOrden.vb
@@ -23,77 +23,22 @@
 
         accesoBD.nonQuery(sentencia)
 
-        'actualiza fecha fin alojamiento
-
-        'sentencia = "UPDATE alojamientos SET fechaFinAlojamiento='" & Date.Today() & "' where idAlojamiento=" & idAlojamiento
-        'accesoBD.nonQuery(sentencia)
-
         'crear detalles de ordenes
         '    a partir de articulos
-        Dim articulos As New DataTable
-
-        sentencia = "SELECT O.idOrden, D.cantidad, D.idRenglon, A.nombre, A.precioUnitario, A.idProveedor, GETDATE()"
-        sentencia &= " FROM OrdenesCompra O JOIN DetallesOrdenes D ON O.idOrden=D.idOrden JOIN Aticulos A ON D.idArticulo=A.idArticulo"
-
-
-        articulos = accesoBD.query(sentencia)
         Dim contador As Integer = 1
+        For i = 0 To tablaDetalles.Rows.Count() - 1
 
-        'If articulos.Rows.Count() > 0 Then
-        '    For i = 0 To consumiciones.Rows.Count() - 1
-        '        Dim fecha As Date = consumiciones.Rows(i)(4)
-        '        sentencia = "INSERT INTO DetallesXFactura (nroFactura,tipoFactura,subtotal,idConsumicion,fechaEmision,idRenglon) "
-        '        sentencia &= "VALUES(" & consumiciones.Rows(i)(0) & "," & consumiciones.Rows(i)(1)
-        '        sentencia &= "," & consumiciones.Rows(i)(2) & "," & consumiciones.Rows(i)(3)
-        '        sentencia &= ",'" & fecha.Date & "', " & (i + 1) & ")"
-        '        contador = contador + 1
-        '        accesoBD.nonQuery(sentencia)
-        '    Next
-        'End If
+            sentencia = "INSERT INTO DetallesOrdenes (idOrden,idArticulo,cantidad,idRenglon)"
+            sentencia &= " VALUES(" & idOrden & "," & tablaDetalles.Rows(i)(1)
+            sentencia &= "," & tablaDetalles.Rows(i)(2) & "," & contador
+            accesoBD.nonQuery(sentencia)
 
-        '    a partir de consumiciones
-        'Dim servicios As New DataTable
+            contador = contador + 1
+        Next
+      
 
-        'sentencia = "SELECT F.nroFactura, F.tipoFactura, S.precioUnitario, C.idServicio, GETDATE()"
-        'sentencia &= " FROM ServiciosXAlojamiento C JOIN Facturas F ON C.idAlojamiento=F.idAlojamiento"
-        'sentencia &= " JOIN Servicios S ON C.idServicio=S.idServicio"
-        'sentencia &= " WHERE F.idAlojamiento=" & idAlojamiento
+        'imprimir orden todo tuyo Juan
 
-        'servicios = accesoBD.query(sentencia)
-
-        'If servicios.Rows.Count() > 0 Then
-        '    For i = 0 To servicios.Rows.Count() - 1
-        '        Dim fecha As Date = servicios.Rows(i)(4)
-        '        sentencia = "INSERT INTO DetallesXFactura (nroFactura,tipoFactura,subtotal,idServicio,fechaEmision,idRenglon)"
-        '        sentencia &= " VALUES(" & servicios.Rows(i)(0) & "," & servicios.Rows(i)(1)
-        '        sentencia &= "," & servicios.Rows(i)(2) & "," & servicios.Rows(i)(3)
-        '        sentencia &= ",'" & fecha.Date & "', " & (contador + 1) & ")"
-        '        contador = contador + 1
-        '        accesoBD.nonQuery(sentencia)
-        '    Next
-        'End If
-
-        'Suma el total de la factura y actualiza
-        sentencia = "SELECT SUM(D.cantidad*A.precioUnitario) FROM DetallesOrdenes D JOIN Articulos A ON D.idArticulo = A.idArticulo WHERE D.idOrden =" & idOrden
-        Dim tabla As DataTable = accesoBD.query(sentencia)
-        Dim subtotal As Double = tabla.Rows(0)(0)
-        'sentencia = "SELECT DATEDIFF(day, fechaInicioAlojamiento, fechaFinAlojamiento), precioPorDia"
-        'sentencia &= " from alojamientos where idAlojamiento=" & idAlojamiento
-        'tabla = accesoBD.query(sentencia)
-        'Dim precioDia As Double = tabla.Rows(0)(1)
-        'Dim cantDias As Integer = tabla.Rows(0)(0)
-        'Dim costoAloj As Double = precioDia * cantDias
-
-        'sentencia = "UPDATE facturas SET total=" & (subtotal + costoAloj) & " where idAlojamiento=" & idAlojamiento
-        'accesoBD.nonQuery(sentencia)
-
-        'imprimir factura
-
-        'sentencia = "SELECT nroFactura FROM Facturas WHERE idAlojamiento=" & idAlojamiento
-        'tabla = accesoBD.query(sentencia)
-        'Dim nro As Integer = tabla.Rows(0)(0)
-        'Dim imprFactura As New Impresion_Factura(nro, idTipoFactura)
-        'imprFactura.ShowDialog()
 
     End Sub
 
@@ -109,7 +54,30 @@
     End Function
 
     Private Sub actualizarStock()
-        'COMPLETAR
+
+        Dim tablaArticulosCantidad As New DataTable
+        Dim sentencia As String = ""
+
+        sentencia = "SELECT D.idArticulo, D.cantidad"
+        sentencia &= " FROM OrdenesCompra O JOIN DetallesOrdenes D ON O.idOrden = D.idOrden"
+        sentencia &= " WHERE O.idOrden=" & idOrden
+
+        tablaArticulosCantidad = accesoBD.query(sentencia)
+
+        If tablaArticulosCantidad.Rows.Count > 0 Then
+
+            For i = 0 To tablaArticulosCantidad.Rows.Count() - 1
+
+                sentencia = "UPDATE articulos SET cantidad = cantidad + " & tablaArticulosCantidad.Rows(i)(1) & " WHERE idArticulo=" & tablaArticulosCantidad.Rows(i)(0)
+                accesoBD.nonQuery(sentencia)
+
+            Next
+
+            sentencia = "UPDATE OrdenesCompra SET fechaRecepcion='" & Date.Today() & "' where idOrden=" & idOrden
+            accesoBD.nonQuery(sentencia)
+
+        End If
+
     End Sub
 
     Public Sub leerOrden()
