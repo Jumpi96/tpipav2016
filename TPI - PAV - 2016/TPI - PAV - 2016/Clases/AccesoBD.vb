9ed12e753f97b2d1cd174c7430b9f59a494ec199
MauriCasares <mauricd@hotmail.com>
2016-10-17 19:39:56 -0300

    transaccion

--- TPI - PAV - 2016/TPI - PAV - 2016/Clases/AccesoBD.vb
+++ TPI - PAV - 2016/TPI - PAV - 2016/Clases/AccesoBD.vb
@@ -1,10 +1,18 @@
 ﻿Public Class AccesoBD
-    Private pCadenaConexion As String
-    Private conexion As Object
-    Private comando As Object
+    Dim pCadenaConexion As String
+    Dim conexion As Object
+    Dim comando As Object
+    Dim transaccion As Object
+    Dim sentenciasTransaccionales As Collection
+
 
     Private Shared _instancia As AccesoBD = Nothing
     Private Shared ReadOnly sync As New Object
+
+    Enum tipo_conexion
+        simple
+        transaccion
+    End Enum
 
 
     'GETTER Y SETTER de cadena conexion
@@ -63,7 +71,7 @@
 
     Public Function query(ByVal sentenciaSQL As String) As DataTable
         Dim retorno As New DataTable
-        Me.conectar()
+        Me.conectar(tipo_conexion.simple)
         Me.comando.CommandType = CommandType.Text
         Me.comando.CommandText = sentenciaSQL
 
@@ -74,21 +82,80 @@
     End Function
 
     Public Sub nonQuery(ByVal sentenciaSQL As String)
-        Me.conectar()
+        Me.conectar(tipo_conexion.simple)
         Me.comando.CommandType = CommandType.Text
         Me.comando.CommandText = sentenciaSQL
         Me.comando.ExecuteNonQuery()
         Me.cerrar()
     End Sub
 
+    Public Function transaction(ByVal primeraSentenciaSQL As String, ByVal segundaSentenciaSQL As String) As String
+        Dim retorno As String = ""
+
+        Me.conectar(tipo_conexion.transaccion)
+        Me.comando.CommandType = CommandType.Text
+
+        Try
+            Me.comando.CommandText = primeraSentenciaSQL
+            Me.comando.ExecuteNonQuery()
+            Me.comando.CommandText = segundaSentenciaSQL
+            Me.comando.ExecuteNonQuery()
+            Me.transaccion.commit()
+        Catch ex As Exception
+            Me.transaccion.rollback()
+            retorno = ex.Message
+        Finally
+            Me.cerrar()
+        End Try
+
+        Return retorno
+    End Function
+
+    Public Function transaction(ByVal sentenciaSQL As String, ByVal commit As Boolean) As String
+        Dim retorno As String = ""
+        sentenciasTransaccionales.Add(sentenciaSQL)
+
+        If commit Then
+            Me.conectar(tipo_conexion.transaccion)
+            Me.comando.CommandType = CommandType.Text
+
+            Try
+                For Each collectionItem As Object In sentenciasTransaccionales
+                    Me.comando.CommandText = collectionItem.ToString
+                    Me.comando.ExecuteNonQuery()
+                Next collectionItem
+
+                Me.transaccion.commit()
+            Catch ex As Exception
+                Me.transaccion.rollback()
+                retorno = ex.Message
+            Finally
+                Me.sentenciasTransaccionales.Clear()
+                Me.cerrar()
+            End Try
+        End If
+        Return retorno
+    End Function
+
+
     Public Sub cerrar()
         Me.conexion.Close()
     End Sub
 
-    Private Sub conectar()
-        Me.conexion.ConnectionString = Me.pCadenaConexion
-        Me.conexion.Open()
-        Me.comando.Connection = Me.conexion
-        Me.comando.CommandType() = CommandType.Text
+    Private Sub conectar(ByVal tipoConexion As tipo_conexion)
+        If Me.conexion.State.ToString <> "Open" Then
+
+            Me.conexion.ConnectionString = Me.pCadenaConexion
+            Me.conexion.Open()
+            Me.comando.Connection = Me.conexion
+            Me.comando.CommandType() = CommandType.Text
+
+            If tipoConexion = tipo_conexion.transaccion Then
+
+                Me.transaccion = Me.conexion.BeginTransaction()
+                Me.comando.Transaction = Me.transaccion
+
+            End If
+        End If
     End Sub
 End Class
