9e30e5a13b4df934252ed28855741a4acfc703b1
GisiGimenez <gisigimenez@gmail.com>
2016-10-31 19:40:10 -0300

    Consumiciones de frigo

---
+++ TPI - PAV - 2016/TPI - PAV - 2016/Listados/FormListadoConsumicionesDeFrigo.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormListadoConsumicionesDeFrigo
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ReportDataSource1 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.DataSet = New TPI___PAV___2016.DataSet()
+        Me.ConsumicionesBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.ConsumicionesTableAdapter = New TPI___PAV___2016.DataSetTableAdapters.ConsumicionesTableAdapter()
+        CType(Me.DataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ConsumicionesBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ReportViewer1
+        '
+        ReportDataSource1.Name = "DataSet1"
+        ReportDataSource1.Value = Me.ConsumicionesBindingSource
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource1)
+        Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "TPI___PAV___2016.ListadoConsumicionesDeFrigo.rdlc"
+        Me.ReportViewer1.Location = New System.Drawing.Point(0, 0)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.Size = New System.Drawing.Size(642, 518)
+        Me.ReportViewer1.TabIndex = 0
+        '
+        'DataSet
+        '
+        Me.DataSet.DataSetName = "DataSet"
+        Me.DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'ConsumicionesBindingSource
+        '
+        Me.ConsumicionesBindingSource.DataMember = "Consumiciones"
+        Me.ConsumicionesBindingSource.DataSource = Me.DataSet
+        '
+        'ConsumicionesTableAdapter
+        '
+        Me.ConsumicionesTableAdapter.ClearBeforeFill = True
+        '
+        'FormListadoConsumicionesDeFrigo
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(643, 518)
+        Me.Controls.Add(Me.ReportViewer1)
+        Me.Name = "FormListadoConsumicionesDeFrigo"
+        Me.Text = "Form1"
+        CType(Me.DataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ConsumicionesBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents ConsumicionesBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents DataSet As TPI___PAV___2016.DataSet
+    Friend WithEvents ConsumicionesTableAdapter As TPI___PAV___2016.DataSetTableAdapters.ConsumicionesTableAdapter
+End Class
---
+++ TPI - PAV - 2016/TPI - PAV - 2016/Listados/FormListadoConsumicionesDeFrigo.vb
@@ -0,0 +1,19 @@
+﻿Public Class FormListadoConsumicionesDeFrigo
+    Dim accesoBD As AccesoBD = AccesoBD.instancia
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.impresion()
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+    Private Sub impresion()
+
+        Dim sql As String = ""
+        sql = "SELECT * FROM CONSUMICIONES WHERE frigobar=1"
+        Dim tabla As DataTable = accesoBD.query(sql)
+        Me.ConsumicionesBindingSource.DataSource = tabla
+
+
+
+    End Sub
+
+End Class
