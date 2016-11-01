3bc8cbccfbc4ac356aea5e60e375bec70dc7ab7d
luchogil96 <luchogil96@gmail.com>
2016-11-01 10:03:24 -0300

    Listado Ordenes de Compra

---
+++ TPI - PAV - 2016/TPI - PAV - 2016/Listados/FormListadoOrdenesCompra.vb
@@ -0,0 +1,10 @@
+﻿Public Class FormListadoOrdenesCompra
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'DataSet.OrdenesCompra' table. You can move, or remove it, as needed.
+        Me.OrdenesCompraTableAdapter.Fill(Me.DataSet.OrdenesCompra)
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+
+End Class
