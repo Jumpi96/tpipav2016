4b5238bcb1c57316cca56d7c6a7925b629c94fcf
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2016-10-31 22:16:39 -0300

    Arreglos listados mios

--- TPI - PAV - 2016/TPI - PAV - 2016/Listados/FormListadoClientes.vb
+++ TPI - PAV - 2016/TPI - PAV - 2016/Listados/FormListadoClientes.vb
@@ -1,9 +1,12 @@
-﻿Public Class FormListadoClientes
+﻿Imports Microsoft.Reporting.WinForms
+
+Public Class FormListadoClientes
 
     Private Sub FormListadoClientes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'DataSet.ListadoClientes' table. You can move, or remove it, as needed.
         Me.ListadoClientesTableAdapter.Fill(Me.DataSet.ListadoClientes)
 
         Me.ReportViewer1.RefreshReport()
+        Me.ReportViewer1.SetDisplayMode(DisplayMode.PrintLayout)
     End Sub
 End Class
--- TPI - PAV - 2016/TPI - PAV - 2016/Listados/FormListadoConsumicionesActuales.vb
+++ TPI - PAV - 2016/TPI - PAV - 2016/Listados/FormListadoConsumicionesActuales.vb
@@ -1,9 +1,12 @@
-﻿Public Class FormListadoConsumicionesActuales
+﻿Imports Microsoft.Reporting.WinForms
+
+Public Class FormListadoConsumicionesActuales
 
     Private Sub FormListadoConsumicionesActuales_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'DataSet.ListadoConsumicionesActuales' table. You can move, or remove it, as needed.
         Me.ListadoConsumicionesActualesTableAdapter.Fill(Me.DataSet.ListadoConsumicionesActuales)
 
         Me.ReportViewer1.RefreshReport()
+        Me.ReportViewer1.SetDisplayMode(DisplayMode.PrintLayout)
     End Sub
 End Class
