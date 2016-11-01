8f573c536c4d40d7fbca589eb40de2ed3090ee95
Juli Rios <julirios299@gmail.com>
2016-11-01 16:35:48 -0300

    sdjh

--- TPI - PAV - 2016/TPI - PAV - 2016/Listados/FormListadoArticulos.Designer.vb
+++ TPI - PAV - 2016/TPI - PAV - 2016/Listados/FormListadoArticulos.Designer.vb
@@ -28,6 +28,9 @@ Partial Class FormListadoArticulos
         Me.DataSet = New TPI___PAV___2016.DataSet()
         Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
         Me.ArticulosTableAdapter = New TPI___PAV___2016.DataSetTableAdapters.ArticulosTableAdapter()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cmb_proveedor = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         CType(Me.ArticulosBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DataSet, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -44,26 +47,54 @@ Partial Class FormListadoArticulos
         '
         'ReportViewer1
         '
-        Me.ReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill
         ReportDataSource1.Name = "DataSet1"
         ReportDataSource1.Value = Me.ArticulosBindingSource
         Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource1)
         Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "TPI___PAV___2016.ListadoArticulos.rdlc"
-        Me.ReportViewer1.Location = New System.Drawing.Point(0, 0)
+        Me.ReportViewer1.Location = New System.Drawing.Point(-3, 33)
         Me.ReportViewer1.Name = "ReportViewer1"
         Me.ReportViewer1.Padding = New System.Windows.Forms.Padding(60, 0, 60, 0)
-        Me.ReportViewer1.Size = New System.Drawing.Size(643, 517)
+        Me.ReportViewer1.Size = New System.Drawing.Size(643, 486)
         Me.ReportViewer1.TabIndex = 0
         '
         'ArticulosTableAdapter
         '
         Me.ArticulosTableAdapter.ClearBeforeFill = True
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Proveedor"
+        '
+        'cmb_proveedor
+        '
+        Me.cmb_proveedor.FormattingEnabled = True
+        Me.cmb_proveedor.Location = New System.Drawing.Point(74, 6)
+        Me.cmb_proveedor.Name = "cmb_proveedor"
+        Me.cmb_proveedor.Size = New System.Drawing.Size(121, 21)
+        Me.cmb_proveedor.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(556, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Filtrar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'FormListadoArticulos
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(643, 517)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.cmb_proveedor)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.ReportViewer1)
         Me.MinimumSize = New System.Drawing.Size(659, 556)
         Me.Name = "FormListadoArticulos"
@@ -71,10 +102,14 @@ Partial Class FormListadoArticulos
         CType(Me.ArticulosBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DataSet, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
     Friend WithEvents ArticulosBindingSource As System.Windows.Forms.BindingSource
     Friend WithEvents DataSet As TPI___PAV___2016.DataSet
     Friend WithEvents ArticulosTableAdapter As TPI___PAV___2016.DataSetTableAdapters.ArticulosTableAdapter
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cmb_proveedor As System.Windows.Forms.ComboBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 End Class
--- TPI - PAV - 2016/TPI - PAV - 2016/Listados/FormListadoArticulos.vb
+++ TPI - PAV - 2016/TPI - PAV - 2016/Listados/FormListadoArticulos.vb
@@ -3,6 +3,8 @@
 Public Class FormListadoArticulos
     Dim accesoBD As AccesoBD = accesoBD.instancia
     Private Sub FormListadoArticulos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.cargar_combo()
+
         Me.impresion()
         Me.ReportViewer1.RefreshReport()
         Me.ReportViewer1.SetDisplayMode(DisplayMode.PrintLayout)
@@ -17,6 +19,23 @@ Public Class FormListadoArticulos
     End Sub
 
     Private Sub ReportViewer1_Load(sender As Object, e As EventArgs) Handles ReportViewer1.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim sql As String = ""
+        sql = "SELECT * FROM ARTICULOS WHERE idproveedor=" & cmb_proveedor.SelectedValue
+        Dim tabla As DataTable = accesoBD.query(sql)
+        Me.ArticulosBindingSource.DataSource = tabla
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+    Private Sub cargar_combo()
+        Dim sentenciaSQL As String = "SELECT * FROM Proveedores"
+        Dim tabla As DataTable = accesoBD.query(sentenciaSQL)
+
+        cmb_proveedor.DataSource = tabla
+        cmb_proveedor.DisplayMember = "nombre"
+        cmb_proveedor.ValueMember = "idProveedor"
 
     End Sub
 End Class
