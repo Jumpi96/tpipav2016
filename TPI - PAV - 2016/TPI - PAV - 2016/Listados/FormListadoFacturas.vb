ccff186c5714d26f3c1dbb00c4284855671d8f98
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2016-11-01 15:34:55 -0300

    FActuras

--- TPI - PAV - 2016/TPI - PAV - 2016/Listados/FormListadoFacturas.vb
+++ TPI - PAV - 2016/TPI - PAV - 2016/Listados/FormListadoFacturas.vb
@@ -1,6 +1,6 @@
 ﻿Public Class FormListadoFacturas
     Dim accesoBD As AccesoBD = accesoBD.instancia
-    Dim flagBusqDocumento As Boolean = False
+    Dim flagBusqDocumento As Integer
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'DataSet.Facturas' table. You can move, or remove it, as needed.
         carga_combo(Me.cmbTipoDoc, Me.leo_tabla("TiposDocumento"), "idTipoDocumento", "nombre")
@@ -12,9 +12,18 @@
     End Function
     Private Sub imprimir()
         Dim sql As String = ""
-        sql &= "SELECT * FROM Facturas"
+        Dim text As String = ""
+        If txtNroDoc.Text = "" Then
+            flagBusqDocumento = 1
+            text = 0
+        Else
+            flagBusqDocumento = 0
+            text = txtNroDoc.Text
+        End If
+        sql &= "SELECT * FROM Facturas F JOIN ALOJAMIENTOS A ON F.IDALOJAMIENTO = A.IDALOJAMIENTO JOIN CLIENTES C ON A.NRODOC = C.NRODOCUMENTO AND A.TIPODOC = C.TIPODOCUMENTO WHERE (C.NRODOCUMENTO= " & text & "AND C.TIPODOCUMENTO=" & cmbTipoDoc.SelectedValue & ") OR 1=" & flagBusqDocumento
         Dim tabla As DataTable = accesoBD.query(sql)
         Me.FacturasBindingSource.DataSource = tabla
+        Me.ReportViewer1.RefreshReport()
     End Sub
     Private Sub carga_combo(ByRef combo As ComboBox, ByRef datos As Data.DataTable, ByVal pk As String, ByVal descripcion As String)
         combo.DataSource = datos
@@ -24,29 +33,7 @@
 
 
     Private Sub btnBuscarPorDoc_Click(sender As Object, e As EventArgs) Handles btnBuscarPorDoc.Click
-        If existeCliente() Then
-            flagBusqDocumento = True
-            Dim sql As String = ""
-            sql &= "SELECT * FROM Facturas F JOIN ALOJAMIENTOS A ON F.IDALOJAMIENTO = A.IDALOJAMIENTO JOIN CLIENTES C ON A.NRODOC = C.NRODOCUMENTO AND A.TIPODOC = C.TIPODOCUMENTO WHERE C.NRODOCUMENTO= " & txtNroDoc.Text & "AND C.TIPODOCUMENTO=" & cmbTipoDoc.SelectedValue
-        Else
-            MessageBox.Show("No existe cliente.", "Error")
-        End If
-        Me.ReportViewer1.RefreshReport()
+        imprimir()
     End Sub
-    Private Function existeCliente() As Boolean
-        Dim sentenciaSQL As String
-        Dim tabla As New DataTable
-
-        If txtNroDoc.Text <> "" Then
-            sentenciaSQL = "select * from Clientes where tipoDocumento =" & cmbTipoDoc.SelectedValue & " AND nroDocumento=" & txtNroDoc.Text
-            tabla = accesoBD.query(sentenciaSQL)
-            Me.FacturasBindingSource.DataSource = tabla
-        End If
-
-        If (tabla.Rows.Count = 0) Then
-            Return False
-        End If
-        Return True
-    End Function
 
 End Class
